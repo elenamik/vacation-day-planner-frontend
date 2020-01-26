eb53c3ac905221107590a79a444a6f9262569459
Elena Mikhaylova <Lena@Mac.lan>
2020-01-26 14:16:25 -0500

    Updates

---
+++ precache-manifest.78bb4ba3bcfc630a125610a721e2f86c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76301c1a3cabc911eb9da1652809930d",
+    "url": "/react-calendar/index.html"
+  },
+  {
+    "revision": "32cdb2f90831a42cd962",
+    "url": "/react-calendar/static/css/main.b5f5a5ec.chunk.css"
+  },
+  {
+    "revision": "ffe7356c063ddb80242c",
+    "url": "/react-calendar/static/js/2.132822ca.chunk.js"
+  },
+  {
+    "revision": "32cdb2f90831a42cd962",
+    "url": "/react-calendar/static/js/main.6657833d.chunk.js"
+  },
+  {
+    "revision": "5c0b7268c8807417032f",
+    "url": "/react-calendar/static/js/runtime~main.292b45be.js"
+  }
+]);
